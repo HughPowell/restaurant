d20561163abf1b0a22fc0f0286001ee14d04e3a9
Hugh Powell <hugh@hughpowell.co.uk>
2024-01-15 22:29:39 +1100

    Simplify the web server implementation

--- src/restaurant.clj
+++ src/restaurant.clj
@@ -1,5 +1,21 @@
 (ns restaurant
   (:require [ring.adapter.jetty :as jetty])
+  (:import (org.eclipse.jetty.server Server))
   (:gen-class))
 
-(defn -main [& _args] (jetty/run-jetty (fn [_] {:status 200 :body "Hello World!"}) {}))
+(defn start-server
+  ([] (start-server {}))
+  ([config] (jetty/run-jetty (fn [_] {:status 200 :body "Hello World!"}) config)))
+
+(defn stop-server [server]
+  (.stop ^Server server))
+
+(defn -main [& _args]
+  (let [server (start-server)]
+    (.addShutdownHook
+      (Runtime/getRuntime)
+      (Thread. ^Runnable (fn [] (stop-server server))))))
+
+(comment
+  (def server (start-server {:port 3000 :join? false}))
+  (stop-server server))
