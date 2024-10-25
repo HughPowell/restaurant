9b5a40f62f290b1eed316b4d86f63b577057fd6c
Hugh Powell <hugh@hughpowell.co.uk>
2024-10-25 20:54:12 +1100

    Encapsulate creating the reservations table

--- infra/data_migrations.clj
+++ infra/data_migrations.clj
@@ -2,6 +2,4 @@
   (:require [restaurant.reservation-book :as reservation-book]))
 
 (defn migrate [_]
-  (reservation-book/execute!
-    reservation-book/reservation-book-config
-    reservation-book/create-reservations-table))
+  (reservation-book/create-reservations-table))
--- src/restaurant/reservation_book.clj
+++ src/restaurant/reservation_book.clj
@@ -6,25 +6,27 @@
 (defprotocol ReservationBook
   (book [this reservation] "Book a new reservation in the reservation book"))
 
-(def reservation-book-config {:dbtype   "postgresql"
-                              :dbname   "restaurant"
-                              :user     "restaurant_owner"
-                              :password (System/getenv "RESTAURANT_DATABASE_PASSWORD")
-                              :host     "ep-shy-boat-a7ii6yjj.ap-southeast-2.aws.neon.tech"
-                              :port     5432})
+(def ^:private reservation-book-config {:dbtype   "postgresql"
+                                        :dbname   "restaurant"
+                                        :user     "restaurant_owner"
+                                        :password (System/getenv "RESTAURANT_DATABASE_PASSWORD")
+                                        :host     "ep-shy-boat-a7ii6yjj.ap-southeast-2.aws.neon.tech"
+                                        :port     5432})
 
-(defn execute! [config sql]
+(defn- execute! [config sql]
   (jdbc/execute!
     (jdbc/get-connection config)
     (sql/format sql)))
 
-(def create-reservations-table
-  {:create-table [:reservations :if-not-exists]
-   :with-columns [[:id :int :generated-always-as-identity :primary-key]
-                  [:at :timestamp-without-time-zone [:not nil]]
-                  [:name [:varchar 50] [:not nil]]
-                  [:email [:varchar 50] [:not nil]]
-                  [:quantity :int [:not nil]]]})
+(defn create-reservations-table []
+  (execute!
+    reservation-book-config
+    {:create-table [:reservations :if-not-exists]
+     :with-columns [[:id :int :generated-always-as-identity :primary-key]
+                    [:at :timestamp-without-time-zone [:not nil]]
+                    [:name [:varchar 50] [:not nil]]
+                    [:email [:varchar 50] [:not nil]]
+                    [:quantity :int [:not nil]]]}))
 
 (def reservation-book
   (reify ReservationBook
