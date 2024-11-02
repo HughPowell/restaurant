a96258dad4913284f03bfe0379eccbcac553e253
Hugh Powell <hugh@hughpowell.co.uk>
2024-11-02 12:49:33 +1100

    Extract creation of reservation test data

--- test/maitre_d_test.clj
+++ test/maitre_d_test.clj
@@ -3,19 +3,22 @@
             [java-time.api :as java-time]
             [restaurant.maitre-d :as sut]))
 
+(defn- reservation
+  ([] (reservation {}))
+  ([updates]
+   (merge {:at       (java-time/local-date-time 2022 04 01 20 15)
+           :email    "x@example.net"
+           :quantity 11}
+          updates)))
+
 (deftest ^:unit accept
 
   (are [maitre-d reserved-seats]
-    (let [existing-reservations (map (fn [quantity]
-                                       {:at       (java-time/local-date-time 2022 04 01 20 15)
-                                        :email    "x@example.net"
-                                        :quantity quantity})
+    (let [existing-reservations (map (fn [quantity] (reservation {:quantity quantity}))
                                      reserved-seats)
-          reservation {:at       (java-time/local-date-time 2022 04 01 20 15)
-                       :email    "x@example.net"
-                       :quantity 11}]
+          proposed-reservation (reservation)]
 
-      (is (sut/will-accept? maitre-d existing-reservations reservation)))
+      (is (sut/will-accept? maitre-d existing-reservations proposed-reservation)))
 
     [{:type :communal :seats 12}] []
     [{:type :communal :seats 8} {:type :communal :seats 11}] []
@@ -24,16 +27,10 @@
 (deftest ^:unit reject
 
   (are [maitre-d reserved-seats]
-    (let [existing-reservations (map (fn [quantity]
-                                       {:at       (java-time/local-date-time 2022 04 01 20 15)
-                                        :email    "x@example.net"
-                                        :quantity quantity})
-                                     reserved-seats)
-          reservation {:at       (java-time/local-date-time 2022 04 01 20 15)
-                       :email    "x@example.net"
-                       :quantity 11}]
+    (let [existing-reservations (map (fn [quantity] (reservation {:quantity quantity})) reserved-seats)
+          proposed-reservation (reservation)]
 
-      (is (not (sut/will-accept? maitre-d existing-reservations reservation))))
+      (is (not (sut/will-accept? maitre-d existing-reservations proposed-reservation))))
 
     [{:type :communal :seats 6} {:type :communal :seats 6}] []
     [{:type :standard :seats 12}] [1]))
